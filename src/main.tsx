6e7c0f9506a144f1fe481df884f563127abf2c95
fernando predes <fernando_predes@hotmail.com>
2022-10-24 12:22:01 -0300

    adicionado o bootstrap aos scripts

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom'
 import router from './router/index'
 import './index.css'
+//bootsrap 5 => documentação em https://react-bootstrap.github.io/
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
