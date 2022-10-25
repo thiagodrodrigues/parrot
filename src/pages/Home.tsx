99f6ef303010926b464ab1b4cd09bb1995b38b00
fernando predes <fernando_predes@hotmail.com>
2022-10-24 22:47:16 -0300

    finalizado estilo do componente Post

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,14 +1,20 @@
 import NavBar from "../components/NavBar"
 import NewPost from "../components/NewPost"
+import vizinhanca from '../assets/faker.json'
+import Post from "../components/Post"
 
 type HomeProps = {}
 
 const Home = (props: HomeProps) => {
+
+
   return (
     <div>
       <NavBar />
-      
+
       <NewPost/>
+      <Post id="1" nome="Jeffrey" ap={231} timestamp="10/06/2022 09:00" post="Poxa galera, vamos respeitar o horário de silêncio!
+Tinha alguém usando furadeira 2 HORAS DA MANHÃ!!! ó_ó" />
     </div>
   )
 }
