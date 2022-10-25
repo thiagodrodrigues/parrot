f790aeee4ce8018953cf7107c0fada634fc43b9f
fernando predes <fernando_predes@hotmail.com>
2022-10-25 13:08:17 -0300

    continuando lógica do Profile

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -18,12 +18,12 @@ const Post = (props: PostProps) => {
   return (
     <PostDetail className='container'>
       <div className="row align-items-center">
-      <img src={avatar} alt="foto de perfil" className=' col-lg-2 col-md-2 col-sm-2 col-3' />
-      <div className='d-flex flex-column col-lg-10 col-md-10 col-sm-10 col-9 '>
-          <Link to={`/${props.id}`}>{props.nome}-{props.ap}</Link>
-          <p>{props.timestamp}</p> <br />
-          <p className='post'>{props.post}</p>
-      </div>
+        <img src={avatar} alt="foto de perfil" className=' col-lg-2 col-md-2 col-sm-2 col-3' />
+        <div className='d-flex flex-column col-lg-10 col-md-10 col-sm-10 col-9 '>
+            <Link to={`/${props.id}`}>{props.nome}-{props.ap}</Link>
+            <p>{props.timestamp}</p> <br />
+            <p className='post'>{props.post}</p>
+        </div>
       </div>
     </PostDetail>
   )
